d70d4c18563e82518dfdba7f3579ae5d0c8b8618
森淼 <wincatcher@qq.com>
2024-10-17 11:29:08 +0800

    优化项目 icon ui 布局

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -55,11 +55,13 @@ export default function Projects({ projectInView, visibleProjects, setVisiblePro
               <div
                 className={`${project.color} p-6 rounded-md border border-gray-300 dark:border-gray-700 h-36 flex flex-col`}
               >
-                <div className="flex items-start flex-grow">
-                  <project.icon className="mr-4 text-2xl flex-shrink-0" />
+                <div className="flex items-center flex-grow">
+                  <div className="mr-4 flex-shrink-0">
+                    <project.icon className="text-2xl" />
+                  </div>
                   <div className="flex-grow">
                     <h2 className="font-bold mb-2">{project.name[locale as keyof typeof project.name]}</h2>
-                    <p className="text-xs mb-2 line-clamp-2">{project.description[locale as keyof typeof project.description]}</p>
+                    <p className="text-xs mb-2 line-clamp-3">{project.description[locale as keyof typeof project.description]}</p>
                   </div>
                 </div>
                 <div className="flex justify-end">
